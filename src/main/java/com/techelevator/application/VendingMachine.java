ee1f01226650d4334f28eb18fa50b8dddb0d65e5
Myrka Gonzalez <myrkag93@gmail.com>
2022-11-18 23:09:40 -0800

    added machinebalance and fixed something in input for money

--- src/main/java/com/techelevator/application/VendingMachine.java
+++ src/main/java/com/techelevator/application/VendingMachine.java
@@ -6,6 +6,7 @@ import com.techelevator.models.TransferFileToList;
 import com.techelevator.ui.UserInput;
 import com.techelevator.ui.UserOutput;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class VendingMachine {
@@ -22,19 +23,26 @@ public class VendingMachine {
                 TransferFileToList list = new TransferFileToList();
                 lisOfItems  = list.readItems();
                 //add if product ran out of stock
+                //maybe we can menu print to its own method
                 for (ItemsForSale item : lisOfItems){
-                    System.out.println(item.getSlot() + " " + item.getItemName() + " " + "$" + item.getPrice() + " quantity left: " + item.getInStock());
+                    System.out.print(item.getSlot() + " " + item.getItemName() + " " + "$" + item.getPrice() + " quantity left: " + item.getInStock());
+                    //System.out.println("itemlength: " + item.getItemName().length());
+                    //System.out.println(item.getSlot() + " " + item.getItemName() + " " + "$" + item.getPrice() + "       " + "Quantity Left: " + item.getInStock());
                 }
             }
             else if(choice.equals("purchase")) {
-                System.out.println("here in purchase");   //so what happened is that we were returning purchase with a capital P in user input so it would exit us
+                System.out.println("here in purchase");
                 choice = userInput.getPurchaseHomeOption();
                 if(choice.equals("Feed money")) {
-                    //CALL BALANCE CLASS SOMEHOW????
-                    Balance.addMoney(); //static worked, now need to figure out how to add the money the user inputs
-                    //Balance.addMoney(); // this doesn't work beacuse static
+                    //LINE 37: SAVES THE BALANCE FROM ADDMONEY() METHOD IN BALANCE CLASS TO VARIABLE X SO WE CAN SEND IT BACK TO BE SAVED IN MACHINE BALANCE
+                    BigDecimal x = Balance.addMoney(); //static worked, now need to figure out how to add the money the user inputs
+                    System.out.println("Current balance in purchase: $" + x);
+                    //THIS IS WHERE WE send x to machineBalance
+                    Balance moneyInMachine = new Balance(x); //THIS CREATES NEW BALANCE OBJECT WHICH WILL HOLD OUR BALANCE
+                    System.out.println("balance class variable machineBalance says amount fed to machine: $" + moneyInMachine.getMachineBalance());
+                    //SO IN PREVIOUS LINE 41 I CALLED THE MACHINE BALANCE AND IT GIVES ME THE SAME NUMBER THAT WE INPUT IN ADDMONEY METHOD
                 }
-                else if(choice.equals("Select item")) {
+                else if(choice.equals("Select item")) { //WE CAN START WORKING ON THIS!!! STEP 7 SELECTION S SELECT ITEM
                     //Bring up item menu?
                 }
                 else if(choice.equals("Finish transaction")) {
@@ -43,10 +51,10 @@ public class VendingMachine {
             }
             else if (choice.equals("exit")) {
                 System.out.println("here in exit");
-                break; // might not need this?
+                break;
             }
         } while(choice.equals("display"));
-        /* //Dont delete this yet in case we need it again because of true condition
+        /* //DONT DELETE THIS YET IN CASE WE NEED IT FOR THE TRUE CONDITION IDK HOW TO EXPLAIN
         while (!x.equals("exit")) {
             //userOutput.displayHomeScreen();
             String choice = userInput.getHomeScreenOption();
--- src/main/java/com/techelevator/models/Balance.java
+++ src/main/java/com/techelevator/models/Balance.java
@@ -1,53 +1,54 @@
 package com.techelevator.models;
 
-import com.techelevator.ui.UserInput;
-
 import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Balance {
-    private BigDecimal currentMoney;
-    private BigDecimal stuffCost;
-    private BigDecimal itemCost;
-    //do we need other variables
+    private BigDecimal machineBalance;
+    private BigDecimal stuffCost; //not used yet
+    private BigDecimal itemCost; //not used yet
 
-    public Balance() {
-        this.currentMoney.equals(BigDecimal.ZERO);
-    }
-    public Balance(BigDecimal itemCost) {
-        this.itemCost.equals(itemCost); //something wrong with this
+    public Balance(BigDecimal x) {
+        this.machineBalance = x; //in order to create a balance object you have to input x which is the current balance
+                                    //Check vending machine class
     }
 
-
-    public BigDecimal getCurrentMoney() {
-        return currentMoney;
+    public BigDecimal getMachineBalance() { //return balance of money fed to machine
+        return machineBalance;
     }
-    //METHOD TO ADD MONEY TO MACHINE
-    //we can simplify this method but idk how to yet
-    public static void addMoney() {
-        System.out.println("Please enter money (only $1, $5, $10 or $20 bills): ");
+
+    public static BigDecimal addMoney() {
         Scanner feedMoney = new Scanner(System.in);
-        BigDecimal money = feedMoney.nextBigDecimal();
         BigDecimal currentMoney = new BigDecimal(0);
-        currentMoney = currentMoney.add(money); //do we need to add this. keyword???
         BigDecimal one = new BigDecimal(1);
-        BigDecimal five = new BigDecimal(5); //creating these variables fixed the issue of not being able to add the user money.
-        BigDecimal ten = new BigDecimal(10);
+        BigDecimal five = new BigDecimal(5); //creating these variables fixed the issue of not being able to add the user money. OKAY.
+        BigDecimal ten = new BigDecimal(10);  //can we put this in private variables at the top? ASK IF THAT IS A GOOD IDEA MAYBE?
         BigDecimal twenty = new BigDecimal(20);
 
-        do {
-            System.out.println("When done entering money please type 'done'"); //add money added for the first time
-            money = feedMoney.nextBigDecimal();
-            if (money.equals(one) || money.equals(five)|| money.equals(ten) || money.equals(twenty)){ // if statement works
+        twoStrings(); //FOR NEATNESS PLEASE ENTER MONEY/TYPE 0 WHEN DONE LINES ADDED TO METHOD AT THE BOTTOM, FEEL FREE TO DELETE THIS COMMENT AFTER YOU READ
+        BigDecimal money = feedMoney.nextBigDecimal();
+
+        while(!money.equals(BigDecimal.ZERO)) {
+            if (money.equals(one) || money.equals(five)|| money.equals(ten) || money.equals(twenty)) { // if statement works
                 currentMoney = currentMoney.add(money);
-                System.out.println("Current Money Provided: " + "$" + currentMoney);
-
-            } else {
-                System.out.println("\nError, enter only $1, $5, $10 or $20 bills.");
+                System.out.println("Current Money Provided: " + currentMoney);
             }
-
-        } while (!money.equals(BigDecimal.ZERO)); //maybe add user input and enter 'quit' in line 37 to quit?
+            else {
+                while(!money.equals(one) || !money.equals(five)|| !money.equals(ten) || !money.equals(twenty)) {
+                    System.out.println("Error, please enter money (only $1, $5, $10 or $20 bills): ");
+                    System.out.println("When done entering money please type '0'");
+                    money = feedMoney.nextBigDecimal();
+                }
+                currentMoney = currentMoney.add(money); //  I DIDNT TRY TO TYPE IN SOMETHING WRONG CUZ WHAT IF IT WENT WRONG AND ITS LIKE 11 FEEL FREE TO TRY
+                System.out.println("Current Money Provided: " + currentMoney);
+            }
+            twoStrings();
+            money = feedMoney.nextBigDecimal();
+        }
+        return currentMoney;
     }
+
+
 
     public void currentMoneyMinusItemPrice() {
         //currentMoney = currentMoney.substract(itemprice); //to update after something is bought
@@ -58,4 +59,11 @@ public class Balance {
     }
     //or can we just use one method and substract from each other
 
+
+
+
+    public static void twoStrings() {
+        System.out.println("Please enter money (only $1, $5, $10 or $20 bills): ");
+        System.out.println("When done entering money please type '0'");
+    }
 }
